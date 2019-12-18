254d4699ca6b5ff9a749226eb57f41b78a53e247
lalize <printf@kakao.com>
2019-12-18 18:01:08 +0900

    feat: 3개의 숫자를 관리할 Balls 클래스 추가

---
+++ src/main/java/domain/ball/Balls.java
@@ -0,0 +1,35 @@
+package domain.ball;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Balls{
+	private static final int SIZE = 3;
+	private static final String OVERLAP_ERROR = "같은 숫자가 존재합니다.";
+	private static final String SIZE_ERROR = SIZE + "자리의 수가 아닙니다.";
+
+	private final List<BallNumber> balls = new ArrayList<>();
+
+	public Balls(List<Integer> numbers) {
+		validate(numbers);
+		numbers.stream().map(BallNumber::new).forEach(balls::add);
+	}
+
+	public static void validate(List<Integer> numbers) {
+		checkOverlap(numbers);
+		checkSize(numbers);
+	}
+
+	private static void checkOverlap(List<Integer> numbers) {
+		if (new HashSet<>(numbers).size() != numbers.size()) {
+			throw new IllegalArgumentException(OVERLAP_ERROR);
+		}
+	}
+
+	private static void checkSize(List<Integer> numbers) {
+		if (numbers.size() != SIZE) {
+			throw new IllegalArgumentException(SIZE_ERROR);
+		}
+	}
+}
