d1556e5f0d0c417092a1cb04d7f8dcbbbc047161
lalize <printf@kakao.com>
2019-12-18 22:14:44 +0900

    feat: 게임 실행 코드 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import controller.BaseballController;
+
 public class Application {
 	public static void main(String[] args) {
-
+		BaseballController baseballController = new BaseballController();
+		baseballController.run();
 	}
 }
